9e1dece2e3a27d9f8180a256156d895632df69db
lucia vilar <vp_lucia@hotmail.com>
2021-06-29 18:16:05 +0200

    not finished

--- js/memory.js
+++ js/memory.js
@@ -1,21 +1,46 @@
 class MemoryGame {
-  constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
-  }
+    constructor(cards) {
+        this.cards = cards;
+        this.pickedCards = [];
+        this.pairsClicked = 0;
+        this.pairsGuessed = 0;
+    }
 
-  shuffleCards() {
-    // ... write your code here
-  }
+    shuffleCards(array) {
 
-  checkIfPair(card1, card2) {
-    // ... write your code here
-  }
+        for (let i = array.length; i > 0; i--) {
+            const shuffle = Math.floor(Math.ramdom() * (i + 1));
+            array[i] = array[shuffle]
 
-  checkIfFinished() {
-    // ... write your code here
-  }
+            return array[shuffle];
+        }
+        if (!array) {
+            return undefined;
+        }
+
+    }
+
+    checkIfPair(card1, card2) {
+
+        this.pairsClicked += 1
+        if (card1 === card2) {
+            this.pairsGuessed += 1
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    checkIfFinished() {
+        if (this.pairGuessed === this.cards.length / 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
